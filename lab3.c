de24fd1487fcaac28a670ebca4a3a375f4575478
Chunxiao523 <cz31@rice.edu>
2017-04-05 23:14:35 -0500

    try

---
+++ lab3.c
@@ -0,0 +1,10 @@
+/*
+* Program for COMP 521 lab3
+* Author: Chunxiao Zhang, Xiangning Qi
+*/
+#include <comp421/filesystem.h>
+#include <comp421/iolib.h>
+#include <stdio.h>
+int main() {
+	return 0;
+}
